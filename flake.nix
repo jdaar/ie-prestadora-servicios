3fba95a51de19dae00ec3872a48c3a38f09a6ada
Jhonatan <jhonatandaar@gmail.com>
2024-08-22 19:19:53 -0500

    chore: cleaned up the sveltekit scaffold

--- flake.nix
+++ flake.nix
@@ -1,10 +1,9 @@
 {
   description = "Devshells & deployment environment";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  inputs.nixvim.url = "github:nix-community/nixvim";
   inputs.nixpkgs.inputs.nixpkgs.url = "nixpkgs";
 
-  outputs = { nixpkgs, nixvim, ... }:
+  outputs = { nixpkgs, ... }:
 	let 
 		system = "x86_64-linux";
 		pkgs = import nixpkgs {
@@ -12,17 +11,12 @@
 			config.allowUnfree = true;
 		};
 		packages = with pkgs; [nodejs_22];
-		nvim = nixvim.legacyPackages.${system}.makeNixvimWithModule {
-			inherit pkgs;
-			module = import ./nvim.nix;
-		};
 	in 
 	{
 		devShells.${system}.default = pkgs.mkShell {
 			inherit system;
 			name = "ie-prestadora-servicios";
 			nativeBuildInputs = packages;
-			buildInputs = [nvim];
 		};
 	};
 }
